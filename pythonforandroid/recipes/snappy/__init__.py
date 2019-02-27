fa3a99999df4c5378133d4d7fadd9059211e390c
opacam <canellestudi@gmail.com>
2019-02-27 01:10:35 +0100

    Build the snappy's recipe using StlRecipe and update version
    Because, this is how we suppose to do so (build the recipe in his own place and link it properly with another recipe in case that we need so). Plus this recipe depends on `c++_shared`, so we inherit from StlRecipe

--- pythonforandroid/recipes/snappy/__init__.py
+++ pythonforandroid/recipes/snappy/__init__.py
@@ -1,13 +1,33 @@
-from pythonforandroid.toolchain import Recipe
+from pythonforandroid.recipe import StlRecipe
+from pythonforandroid.logger import shprint
+from pythonforandroid.util import current_directory
+from os.path import join
+import sh
 
 
-class SnappyRecipe(Recipe):
-    version = '1.1.3'
-    url = 'https://github.com/google/snappy/releases/download/{version}/snappy-{version}.tar.gz'
+class SnappyRecipe(StlRecipe):
+    version = '1.1.7'
+    url = 'https://github.com/google/snappy/archive/{version}.tar.gz'
 
     def should_build(self, arch):
-        # Only download to use in leveldb recipe
-        return False
+        return not self.has_libs(arch, 'libsnappy.so')
+
+    def build_arch(self, arch):
+        super(SnappyRecipe, self).build_arch(arch)
+        env = self.get_recipe_env(arch)
+        source_dir = self.get_build_dir(arch.arch)
+        with current_directory(source_dir):
+            shprint(sh.cmake, source_dir,
+                    '-DANDROID_ABI={}'.format(arch.arch),
+                    '-DANDROID_NATIVE_API_LEVEL={}'.format(self.ctx.ndk_api),
+                    '-DANDROID_STL=' + self.stl_lib_name,
+                    '-DCMAKE_TOOLCHAIN_FILE={}'.format(
+                        join(self.ctx.ndk_dir, 'build', 'cmake',
+                             'android.toolchain.cmake')),
+                    '-DBUILD_SHARED_LIBS=1',
+                    _env=env)
+            shprint(sh.make, _env=env)
+            self.install_libs(arch, 'libsnappy.so')
 
 
 recipe = SnappyRecipe()
